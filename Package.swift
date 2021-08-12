de296f87ce15ffd9288ba144bc92b9eff0ffb7a8
sai <>
2021-08-12 10:08:54 +0800

    test

--- Package.swift
+++ Package.swift
@@ -21,6 +21,9 @@ let package = Package(
         .target(
             name: "LSAttributedLabelLabel",
             dependencies: [],
-            publicHeadersPath: "include"),
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+            ]),
     ]
 )
